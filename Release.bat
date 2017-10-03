8ad1f4da758a4abf265002de56a505292ba51cf2
Ryan Fleming <rfleming@vistaprint.com>
2017-10-03 13:25:51 -0400

    Finish moving around new spec. Remove structuremap project

--- Release.bat
+++ Release.bat
@@ -7,4 +7,3 @@ SET MSBUILD_PATH="C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe"
 %NUGET_PATH% pack Interfaces\JungleBus.Interfaces.nuspec
 %NUGET_PATH% pack JungleBus\JungleBus.nuspec
 %NUGET_PATH% pack JungleBus.Testing\JungleBus.Testing.nuspec
-%NUGET_PATH% pack JungleBus\JungleBus.StructureMap.nuspec
