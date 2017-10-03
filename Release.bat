c3c148885bb1f6ffdfae33ff7b7a9d45dfb7e841
Ryan Fleming <rfleming@vistaprint.com>
2017-10-03 13:21:58 -0400

    Moving around nuspec files

--- Release.bat
+++ Release.bat
@@ -4,6 +4,7 @@ SET MSBUILD_PATH="C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe"
 %MSBUILD_PATH% JungleBus.sln /t:Clean /p:Configuration=Release
 %MSBUILD_PATH% JungleBus.sln /t:Build /p:Configuration=Release
 
-%NUGET_PATH% pack JungleBus.Interfaces.nuspec
-%NUGET_PATH% pack JungleBus.nuspec
-%NUGET_PATH% pack JungleBus.StructureMap.nuspec
+%NUGET_PATH% pack Interfaces\JungleBus.Interfaces.nuspec
+%NUGET_PATH% pack JungleBus\JungleBus.nuspec
+%NUGET_PATH% pack JungleBus.Testing\JungleBus.Testing.nuspec
+%NUGET_PATH% pack JungleBus\JungleBus.StructureMap.nuspec
